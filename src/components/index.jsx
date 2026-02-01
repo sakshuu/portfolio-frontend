42e28f848050b1ee716511401d1d9a7d32366a12
sakshi <sakshisjadhav.120@gmail.com>
2026-02-01 23:57:21 +0530

    updated files

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,4 +2,3 @@ export { default as MyNavbar  } from "./MyNavbar";
 export { default as Experience  } from "./Experience";
 export { default as Qualifications  } from "./Qualifications";
 export { default as ContactMe } from "./ContactMe";
-export { default as UI } from "./UI";
